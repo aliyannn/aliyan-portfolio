ec1865bb4691050e035d3499623d48fd9e0f16ac
aliyannn <aliyangohar00@outlook.com>
2025-06-27 07:51:22 +0500

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,8 @@ function App() {
     return () => clearTimeout(timer);
   }, [])
 
+  
+
   return (
     <Router>
       <Preloader load={load} />
--- src/components/MouseSmokeEffect.jsx
+++ src/components/MouseSmokeEffect.jsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { useCallback, useState } from "react";
 
 function SmokeEffect({ color = "#ffffff", particlesPerEmit = 2 }) {
