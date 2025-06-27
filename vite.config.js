4b4f4d7d1347428320295319fe1c38002a9a343f
aliyannn <aliyangohar00@outlook.com>
2025-06-28 00:30:58 +0500

    add prerender plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { prerender } from 'vite-plugin-prerender';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), visualizer()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    visualizer(),
+    prerender({
+      routes: ['/', '/about', '/projects', '/contact'],
+    }),
+  ],
   base: '/',
-})
+});
