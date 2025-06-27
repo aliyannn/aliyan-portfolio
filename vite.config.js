602efdba3c650f5068866e4cb1efa8d89eeb4d00
aliyannn <aliyangohar00@outlook.com>
2025-06-28 00:37:26 +0500

    update SEO

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { prerender } from 'vite-plugin-prerender';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
     visualizer(),
-    prerender({
-      routes: ['/', '/about', '/projects', '/contact'],
-    }),
   ],
   base: '/',
 });
